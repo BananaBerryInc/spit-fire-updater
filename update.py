ace6183921d6a7fe51fbd09a395fd883877eb8b3
Johnathan Warawa <johnathan8080@gmail.com>
2017-06-14 15:14:47 -0600

    Windows support

--- update.py
+++ update.py
@@ -1,4 +1,6 @@
 import git
 from subprocess import call
 call(["rm","-rf", "-r", "spit-fire"])
+call(["del","spit-fire/*.*"])
+call(["del","spit-fire\*.*"])
 git.Git().clone("https://github.com/BananaBerryInc/spit-fire")
